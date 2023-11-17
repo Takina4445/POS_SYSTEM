d27a0211183d085832b1f506bd0723b690a94651
Yuuki <s47161261@gmail.com>
2023-11-17 13:50:41 +0800

    新增log.db資料庫、更新"輸入資料按鈕"內功能(完成對db資料庫新增資料)、新增"更新資料按鈕"(完成對db資料庫內資料的修改)

--- 20231013/Form1.Designer.cs
+++ 20231013/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -73,7 +74,7 @@
             this.button1.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button1.Location = new System.Drawing.Point(96, 479);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 41);
+            this.button1.Size = new System.Drawing.Size(79, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "輸入資料";
             this.button1.UseVisualStyleBackColor = true;
@@ -82,9 +83,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(251, 479);
+            this.button2.Location = new System.Drawing.Point(263, 479);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 41);
+            this.button2.Size = new System.Drawing.Size(80, 41);
             this.button2.TabIndex = 1;
             this.button2.Text = "輸出資料";
             this.button2.UseVisualStyleBackColor = true;
@@ -288,6 +289,7 @@
             this.dataGridView1.RowTemplate.Height = 27;
             this.dataGridView1.Size = new System.Drawing.Size(935, 457);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // Column1
             // 
@@ -375,11 +377,23 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(177, 479);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 41);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "更新資料";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1490, 669);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.comboBox1);
@@ -445,6 +459,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
