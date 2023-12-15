ecbf17f0319a59da60dc7c4bef654ab50780599c
s47161261@gmail.com <User@F602-34>
2023-12-15 12:20:11 +0800

    新增"輸出Excel"按鈕

--- 20231013/Form1.Designer.cs
+++ 20231013/Form1.Designer.cs
@@ -61,6 +61,7 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -262,6 +263,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button4);
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
@@ -287,7 +289,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(935, 457);
+            this.dataGridView1.Size = new System.Drawing.Size(935, 406);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
@@ -388,6 +390,16 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 416);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 38);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "輸出EXCEL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -460,6 +472,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- 20231013/Form1.cs
+++ 20231013/Form1.cs
@@ -204,6 +204,55 @@ namespace _20231013
             Show_DB();
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            save.FileName = "Export_Data";
+            save.Filter = "*.xlsx|*.xlsx";
+            if (save.ShowDialog() != DialogResult.OK) return;
+
+            // Excel 物件
+            Microsoft.Office.Interop.Excel.Application xls = null;
+            try
+            {
+                xls = new Microsoft.Office.Interop.Excel.Application();
+                // Excel WorkBook
+                Microsoft.Office.Interop.Excel.Workbook book = xls.Workbooks.Add();
+                //Excel.Worksheet Sheet = (Excel.Worksheet)book.Worksheets[1];
+                Microsoft.Office.Interop.Excel.Worksheet Sheet = xls.ActiveSheet;
+
+                // 把 DataGridView 資料塞進 Excel 內
+
+                // DataGridView 標題
+                for (int k = 0; k < this.dataGridView1.Columns.Count; k++)
+                {
+                    Sheet.Cells[1, k + 1] = this.dataGridView1.Columns[k].HeaderText.ToString();
+                }
+
+                // DataGridView 內容
+                for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < this.dataGridView1.Columns.Count; j++)
+                    {
+                        string value = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                        Sheet.Cells[i + 2, j + 1] = value;
+                    }
+                }
+
+                // 儲存檔案
+                book.SaveAs(save.FileName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                xls.Quit();
+            }
+
+        }
     }
 }
 
